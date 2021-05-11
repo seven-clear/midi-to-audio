471de0bc717980053d8f61a6709bb5ab40ec4e96
weizhensheng <957348729@qq.com>
2021-05-11 15:31:47 +0800

    feat:add master gain parameter

--- midi2audio.py
+++ midi2audio.py
@@ -10,8 +10,8 @@ class FluidSynth():
     def __init__(self, sound_font=DEFAULT_SOUND_FONT):
         self.sound_font = os.path.expanduser(sound_font)
 
-    def midi_to_audio(self, midi_file, audio_file, sample_rate=DEFAULT_SAMPLE_RATE):
-        cmd = f"fluidsynth -ni -F {audio_file} -r {str(sample_rate)} {self.sound_font} {midi_file}"
+    def midi_to_audio(self, midi_file, audio_file, gain=0.2, sample_rate=DEFAULT_SAMPLE_RATE):
+        cmd = f"fluidsynth -ni -F {audio_file} -r {str(sample_rate)} -g {gain} {self.sound_font} {midi_file}"
         os.system(cmd)
 
     def play_midi(self, midi_file, sample_rate=DEFAULT_SAMPLE_RATE):
